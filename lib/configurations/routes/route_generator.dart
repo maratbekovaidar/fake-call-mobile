eca60f7c44148cfddcd05fe81db70f48242c0c18
maratbekovaidar <72737452+maratbekovaidar@users.noreply.github.com>
2022-05-02 02:38:49 +0600

    feat(route_generator): create route_generator.dart_

---
+++ lib/configurations/routes/route_generator.dart
@@ -0,0 +1,31 @@
+
+import 'package:fake_call_mobile/features/views/main/screens/main_page.dart';
+import 'package:flutter/material.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+
+    // ignore: unused_local_variable
+    final args = settings.arguments;
+
+    switch (settings.name) {
+      case MainPage.route:
+        return MaterialPageRoute(builder: (_) => const MainPage());
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Error'),
+        ),
+        body: const Center(
+          child: Text('ERROR'),
+        ),
+      );
+    });
+  }
+}
---
+++ lib/configurations/themes/app_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+/// All custom application theme
+class AppTheme {
+
+  /// Default application theme
+  static ThemeData get basic => ThemeData(
+
+  );
+
+}
