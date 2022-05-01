3eb80b6d24b37a20f6b7571cfbcd0cb712b4f83a
maratbekovaidar <72737452+maratbekovaidar@users.noreply.github.com>
2022-05-02 03:24:37 +0600

    feat(themes): add theme for input and button

--- lib/configurations/themes/app_theme.dart
+++ lib/configurations/themes/app_theme.dart
@@ -6,6 +6,56 @@ class AppTheme {
   /// Default application theme
   static ThemeData get basic => ThemeData(
 
+    /// TextField Theme
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: const BorderSide(color: Colors.black12, width: 1),
+      ),
+      disabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: const BorderSide(color: Colors.black12, width: 1),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: const BorderSide(color: Colors.black12, width: 1),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: const BorderSide(color: Colors.red, width: 1),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: const BorderSide(color: Colors.lightBlueAccent, width: 1),
+      ),
+      fillColor: Colors.white,
+      filled: true,
+    ),
+
+
+    /// Button Theme
+    elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          primary: Colors.redAccent,
+        ).merge(
+          ButtonStyle(
+              elevation: MaterialStateProperty.all(0),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  )
+              ),
+              padding: MaterialStateProperty.all(
+                  const EdgeInsets.symmetric(
+                      vertical: 20,
+                      horizontal: 25
+                  )
+              )
+          ),
+        )
+    ),
+
   );
 
 }
