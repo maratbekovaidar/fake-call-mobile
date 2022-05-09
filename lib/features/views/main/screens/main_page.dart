47e2bf9b8d635ba29abe38769cdde1976f4eb094
maratbekovaidar <72737452+maratbekovaidar@users.noreply.github.com>
2022-05-10 01:19:33 +0600

    [#9] feat(main_page): implement bottom navigation bar

--- lib/features/views/main/screens/main_page.dart
+++ lib/features/views/main/screens/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
@@ -11,6 +12,7 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
 
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +29,33 @@ class _MainPageState extends State<MainPage> {
         child: PageView(
         ),
       ),
+
+      /// Bottom Navigation Bar
+      bottomNavigationBar: BottomNavyBar(
+        animationDuration: const Duration(milliseconds: 300),
+        selectedIndex: _currentIndex,
+        onItemSelected: (index) {
+          setState(() => _currentIndex = index);
+        },
+        items: <BottomNavyBarItem>[
+          BottomNavyBarItem(
+              title: const Text('Home'),
+              icon: const Icon(Icons.home_rounded)
+          ),
+          BottomNavyBarItem(
+              title: const Text('Friends'),
+              icon: const Icon(Icons.person_add_alt_1_rounded)
+          ),
+          BottomNavyBarItem(
+              title: const Text('Location'),
+              icon: const Icon(Icons.location_on_rounded)
+          ),
+          BottomNavyBarItem(
+              title: const Text('Profile'),
+              icon: const Icon(Icons.person_rounded)
+          ),
+        ],
+      )
     );
   }
 }
